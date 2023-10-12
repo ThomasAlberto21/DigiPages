907e8bd165aa35580530751e27cc359d705931a6
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-10-12 19:06:02 +0700

    Start

--- lib/GetStarted.dart
+++ lib/GetStarted.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:digipages/onboarding/start.dart';
 
@@ -6,49 +7,23 @@ class GetStarted extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Timer(const Duration(seconds: 3), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => const Start()));
+    });
+
     return Scaffold(
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         color: Colors.blue[700],
         child: Stack(
-          children: [
+          children: <Widget>[
             Center(
               child: Image.asset(
                 "images/logo.png",
                 height: 300,
                 width: 300,
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 20.0, vertical: 60.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => const Start()));
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.white),
-                    padding: MaterialStateProperty.all(
-                      const EdgeInsets.symmetric(
-                          horizontal: 120.0, vertical: 20.0),
-                    ),
-                    shape: MaterialStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.0),
-                      ),
-                    ),
-                  ),
-                  child: const Text(
-                    "Get Started",
-                    style: TextStyle(
-                      color: Colors.black,
-                    ),
-                  ),
-                ),
               ),
             ),
           ],
--- lib/onboarding/start.dart
+++ lib/onboarding/start.dart
@@ -12,7 +12,10 @@ class Start extends StatelessWidget {
         color: Colors.white,
         child: Stack(
           children: [
-            Center(
+            const SizedBox(height: 100.0),
+            Positioned(
+              top: MediaQuery.of(context).size.height / 4,
+              left: MediaQuery.of(context).size.width / 2 - 150,
               child: Image.asset(
                 "images/start.png",
                 height: 300,
@@ -30,19 +33,22 @@ class Start extends StatelessWidget {
                       "Temukan barang yang anda inginkan disekitar mu",
                       textAlign: TextAlign.center,
                       style: TextStyle(
-                          fontSize: 22.0,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.black),
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black,
+                      ),
                     ),
                     const SizedBox(height: 10.0),
                     const Text(
                       "Aplikasi untuk mencari dan menemukan barang yang kita inginkan di sekitar kita.",
                       textAlign: TextAlign.center,
                       style: TextStyle(
-                          fontSize: 13.0,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.black),
+                        fontSize: 13.0,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.black54,
+                      ),
                     ),
+                    const SizedBox(height: 60.0),
                     Padding(
                       padding: const EdgeInsets.symmetric(
                           horizontal: 20.0, vertical: 40.0),
@@ -53,7 +59,9 @@ class Start extends StatelessWidget {
                               MaterialStateProperty.all(Colors.blue[700]),
                           padding: MaterialStateProperty.all(
                             const EdgeInsets.symmetric(
-                                horizontal: 140.0, vertical: 20.0),
+                              horizontal: 140.0,
+                              vertical: 17.0,
+                            ),
                           ),
                           shape: MaterialStateProperty.all(
                             RoundedRectangleBorder(
@@ -64,7 +72,9 @@ class Start extends StatelessWidget {
                         child: const Text(
                           "Mulai",
                           style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold),
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
                     ),

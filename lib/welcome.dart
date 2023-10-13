64bc141d04f4fdadfbdcfb32c9eca8be313b03fa
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-10-13 21:40:05 +0700

    Welcome

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,27 +1,103 @@
-import 'package:flutter/material.dart';
+  import 'package:flutter/material.dart';
 
-class Welcome extends StatelessWidget {
-  const Welcome({Key? key}) : super(key: key);
+  class Welcome extends StatelessWidget {
+    const Welcome({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        color: Colors.white,
-        child: Stack(
-          children: <Widget>[
-            Center(
-              child: Image.asset(
-                "images/logo2.png",
-                height: 300,
-                width: 300,
+    @override
+    Widget build(BuildContext context) {
+      Color blue = const Color(0xff2563eb);
+      return Scaffold(
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          color: Colors.white,
+          child: Stack(
+            children: <Widget>[
+              Center(
+                child: Image.asset(
+                  "images/logo2.png",
+                  height: 300,
+                  width: 300,
+                ),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 10.0, vertical: 8.0),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(blue),
+                            padding: MaterialStateProperty.all(
+                              const EdgeInsets.symmetric(
+                                horizontal: 140.0,
+                                vertical: 20.0,
+                              ),
+                            ),
+                            shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30.0),
+                              ),
+                            ),
+                          ),
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 10.0, vertical: 8.0),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.white),
+                            padding: MaterialStateProperty.all(
+                              const EdgeInsets.symmetric(
+                                horizontal: 130.0,
+                                vertical: 20.0,
+                              ),
+                            ),
+                            shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30.0),
+                              ),
+                            ),
+                            side: MaterialStateProperty.all(
+                              BorderSide(
+                                color: blue,
+                                width: 2.0,
+                              ),
+                            ),
+                          ),
+                          child: Text(
+                            "Register",
+                            style: TextStyle(
+                              color: blue,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 30.0),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    }
   }
-}
